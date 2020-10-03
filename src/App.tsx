94f6a3340e6685dc71942f04559be68eff5a2801
maksim-khiuttiulia <43935623+maksim-khiuttiulia@users.noreply.github.com>
2020-10-03 17:31:08 +0200

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import {SafeAreaView, Text} from "react-native";
+
+
+class App extends React.Component<any, any> {
+  render() {
+    return (
+        <SafeAreaView>
+          <Text>
+            Sukaaaaa
+          </Text>
+        </SafeAreaView>
+    )
+  }
+}
+
+export default App;
